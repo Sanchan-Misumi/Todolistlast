738977233fae131f9a26a4b1fa72b094ca988889
Maho Misumi <maho.smile606@gmail.com>
2017-12-04 11:38:09 +0900

    接続が汚い

--- ToDoListLast/AddViewController.swift
+++ ToDoListLast/AddViewController.swift
@@ -10,41 +10,45 @@ import UIKit
 
 
 class AddViewController: UIViewController {
+    
     //ToDoItemという変数を用意します
     var todoItem = [String]()
     
-  
-
+    //倉庫を作ってそこにデータ保存します
+    let saveDate: UserDefaults = UserDefaults.standard
+    
     //UITextField!使います
     @IBOutlet var todoTextField: UITextField!
     
-    //倉庫を作ってそこにデータ保存します
-    let saveDate: UserDefaults = UserDefaults.standard
-
+    //この画面を開くときにする動作
+    override func viewDidLoad() {
+        super.viewDidLoad()
+          // Do any additional setup after loading the view.
+        //もしUserDefaultsのforKey: "Todo"の中身が空ではなければ、変数todoItemにUserDefaultsのforKey: "Todoの中の配列の中身を代入します
+        if UserDefaults.standard.array(forKey: "Todo") != nil{
+            todoItem = UserDefaults.standard.array(forKey: "Todo") as! [String]
+        }
+    }
     
     //addItemを押した時にUserDefaultsにデータを保存します
     @IBAction func addItem(){
+        //変数todoItemの中にtodoTextFieldに書いたテキストを追加します
         todoItem.append(todoTextField.text!)
+         //変数todoItemの中身を UserDefaultsに保存します
         saveDate.set(todoItem, forKey: "Todo")
+        //todoItemの中身をコンソールに代入します
         print(todoItem)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //これ何？
-        if UserDefaults.standard.array(forKey: "Todo") != nil{
-            todoItem = UserDefaults.standard.array(forKey: "Todo")as! [String]
-        }
-        
-        // Do any additional setup after loading the view.
-    }
-
+     //
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
     func textFieldShouldReturn(textField: UITextField!) -> Bool{
         todoTextField.resignFirstResponder()
         return true
